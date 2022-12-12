e30b6bfd754f15b9136dcf1326aecb955f9fee3f
Ahmedish <yamood02@gmail.com>
2022-12-11 19:59:17 -0500

    Albums can now be selected and edited

--- app/src/main/java/com/example/photos32/Albums.java
+++ app/src/main/java/com/example/photos32/Albums.java
@@ -22,22 +22,16 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Albums extends AppCompatActivity {
-
-    private ArrayList<Album> albums;
     private ListView listView;
+    private ArrayList<Album> albums;
     private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.albums_list);
-
-        listView = findViewById(R.id.albums_list);
-
         path = this.getApplicationInfo().dataDir + "/data.dat";
-
         File data = new File(path);
-
         if (!data.isFile() || !data.exists()) {
             try {
                 data.createNewFile();
@@ -47,8 +41,6 @@ public class Albums extends AppCompatActivity {
                 exception.printStackTrace();
             }
         }
-
-        // File exists, proceed to read it
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
@@ -62,8 +54,10 @@ public class Albums extends AppCompatActivity {
         //populate the list view
         ArrayAdapter<Album> adapter = new ArrayAdapter<>(this, R.layout.album, albums);
         adapter.setNotifyOnChange(true);
+        listView = findViewById(R.id.albums_list);
         listView.setAdapter(adapter);
-
+        listView.setChoiceMode(1);
+        listView.setItemsCanFocus(false);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -72,21 +66,17 @@ public class Albums extends AppCompatActivity {
         });
 
     }
-
     public void addAlbum(View view) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog.Builder b = new AlertDialog.Builder(this);
         final ArrayAdapter<Album> adapter = (ArrayAdapter<Album>) listView.getAdapter();
         final EditText input = new EditText(this);
-
         input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setTitle("Enter a name for the album");
-        builder.setView(input);
-
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+        b.setTitle("Enter a name for the album");
+        b.setView(input);
+        b.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Album newAlbum = new Album(input.getText().toString());
-
                 adapter.add(newAlbum);
                 ArrayList<Album> al = new ArrayList<>();
                 for (int i = 0; i < adapter.getCount(); i++) {
@@ -95,16 +85,51 @@ public class Albums extends AppCompatActivity {
                 DataHelper.save(al, path);
             }
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
-
-        AlertDialog dialog = builder.create();
-        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-        dialog.show();
+        AlertDialog d = b.create();
+        d.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        d.show();
+    }
+    public void editAlbum(View view) {
+        Object o = listView.getItemAtPosition(listView.getCheckedItemPosition());
+        if (o == null) {
+            //error
+            System.out.println("nothing selected");
+            return;
+        }
+        Album a = (Album) o;
+        final AlertDialog.Builder b = new AlertDialog.Builder(this);
+        final ArrayAdapter<Album> adapter = (ArrayAdapter<Album>) listView.getAdapter();
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setText(a.getName());
+        b.setTitle("Enter a new name for the album");
+        b.setView(input);
+        b.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                a.setName(input.getText().toString());
+                ArrayList<Album> al = new ArrayList<>();
+                for (int i = 0; i < adapter.getCount(); i++) {
+                    al.add(adapter.getItem(i));
+                }
+                DataHelper.save(al, path);
+            }
+        });
+        b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog d = b.create();
+        d.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        d.show();
     }
 
 }
