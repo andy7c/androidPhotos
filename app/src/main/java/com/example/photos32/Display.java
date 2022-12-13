32e88f5b2fa043677e9f6310c67be330db4f25bc
Ahmedish <yamood02@gmail.com>
2022-12-12 22:04:39 -0500

    cleaned up display view

--- app/src/main/java/com/example/photos32/Display.java
+++ app/src/main/java/com/example/photos32/Display.java
@@ -1,18 +1,8 @@
 package com.example.photos32;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Checkable;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,7 +24,7 @@ public class Display extends AppCompatActivity {
         Photo p = photos.get(position);
         ImageView image = findViewById(R.id.picture);
         image.setImageBitmap(p.image.getBitmap());
-        TextView caption = findViewById(R.id.caption);
+        TextView caption = findViewById(R.id.captionText);
         caption.setText("asdf");
 
 
